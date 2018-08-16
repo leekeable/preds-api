e9b410698ed06edf4930a4e31f61057d98bda5a4
leekeable <lee.keable@gmail.com>
2018-08-16 15:12:38 +0100

    remove additional api from the path

--- FixtureService/Controllers/FixturesController.cs
+++ FixtureService/Controllers/FixturesController.cs
@@ -4,7 +4,7 @@
     using System;
     using System.Collections.Generic;
 
-    [Route("api")]
+    [Route("/")]
     [ApiController]
     public class FixturesController : FixtureServiceControllerBase
     {
